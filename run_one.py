d32d6675e5fec9f32e328ed561a697d8a719b09e
Wojciech Dubiel <wojciech@golem.network>
2017-08-28 13:39:43 +0200

    Prepare run_one.py for irl tests.

--- run_one.py
+++ run_one.py
@@ -4,4 +4,7 @@ import sys
 from playground.run import run
 from playground.app import PlaygroundApp, PlaygroundService
 
-run(PlaygroundApp, PlaygroundService, num_nodes=1, all_nodes=10, max_peers=8, min_peers=2, bootstrap_nodes=[x.encode() for x in sys.argv[1:]])
+my_node_num=CHANGEME
+seed=0
+
+run(PlaygroundApp, PlaygroundService, num_nodes=1, start_num=my_node_num, seed=seed, all_nodes=20, max_peers=19, min_peers=7, bootstrap_nodes=[b'enode://316d88516ff96ed22251f178684c6ea296d224be06e7ee301d363abd03aa9ad757b0fb647ad726e2367750e0f30c7240b010efe87a1ae9c5e9896f78e8827aff@52.91.184.235:29870'])
